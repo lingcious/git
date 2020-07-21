2304ddc803e71ebbd9d8ac76ab610793327b1042
lingcious <3479708118@qq.com>
2020-07-22 01:19:20 +0800

    再次更新

--- src/git/gittest.java
+++ src/git/gittest.java
@@ -3,6 +3,6 @@ package git;
 public class gittest {
 public static void main(String[] args) {
 	System.out.println("hello git hub");
-	System.out.println("我是更新的部分");
+	System.out.println("我是再次更新的部分");
 }
 }
